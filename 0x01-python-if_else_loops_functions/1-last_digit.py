369da97dc1955e304629b9747ce3349139910e10
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-11-29 16:01:44 -0800

    fix(0x01): task 1, handle the sign of the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = int(str(number)[-1])
-print("Last digit of {:d} is {:d}".format(number, lastDigit), end = " ")
-print(f"and is greater than 5" if lastDigit > 5 else
-       "and is less than 6 and not 0" if lastDigit < 6 and lastDigit != 0 else
-       "and is 0")
+lastDigit = abs(number) % 10 * (-1 if number < 0 else 1)
+
+print("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
+if lastDigit > 5:
+    print("and is greater than 5")
+elif lastDigit < 6 and lastDigit != 0:
+    print("and is less than 6 and not 0")
+else:
+    print("and is 0")
