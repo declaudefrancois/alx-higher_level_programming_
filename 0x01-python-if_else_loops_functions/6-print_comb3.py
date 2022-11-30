b29f315ee9e89ddb027541620a7710e189394a7a
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-11-29 17:03:47 -0800

    feat(0x01): task 6. prints all possible different combinations of two digits.

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if (j == i):
+            continue
+
+        if (i == 8 and j == 9):
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
