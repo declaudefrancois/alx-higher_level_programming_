1342a171b97fad64b1f5e94620949b16f36eaacd
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-01 15:57:52 -0800

    fix(0x02): handle plural when displaying arguments number

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{:d} arguments{:s}".format(len(argv) - 1,
+    print("{:d} argument{:s}{:s}".format(len(argv) - 1,
+          "" if len(argv) == 2 else "s",
           "." if len(argv) == 1 else ":"))
 
     for i in range(1, len(argv)):
