2f940deacbc02c18a0ac4f7dbe0167dc5e5aa8a4
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-02 00:28:18 +0100

    feat(0x02): import and use a function from a module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Adds two integer.
+
+    Args:
+        a: first integer.
+        b: second integer.
+
+    Returns:
+        Return the result of a + b
+    """
+    return a + b
