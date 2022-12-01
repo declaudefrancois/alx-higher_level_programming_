e5c53de2ff7c26c2e30c7d8dd1a78f35086c7b98
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-02 00:30:49 +0100

    feat(0x02): import and uses multiples function from a module. And make the code not executable when imported

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Adds two integers
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sesult of a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """Substracts the second integer from the first.
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sesult of a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """Multiply two integers.
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sesult of a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """Substracts the second integer from the first.
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The sesult of a / b
+    """
+    return (a - b)
+
