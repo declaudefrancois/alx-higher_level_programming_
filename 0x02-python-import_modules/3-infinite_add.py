2164303eb19f0a8bee4b29d3e943414a1b5d0a47
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-02 00:34:58 +0100

    feat(0x02): calculate the sum of all command line arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    def add_recursive(a, i):
+        """Recursively adds numbers in the list a.
+
+        Args:
+            a: a list of integers.
+            i: the index of the first item to sum.
+
+        Returns:
+            sum of all items of a.
+        """
+        if i >= len(a):
+            return (0)
+
+        return (int(a[i]) if i == len(argv) else 
+                int(a[i]) + add_recursive(a, i + 1))
+
+    print("{:d}".format(add_recursive(argv, 1)))
