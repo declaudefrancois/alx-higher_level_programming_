245bbde3e6bea7859528d5019815c964bcd84946
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-01 17:24:51 -0800

    feat(0x02): prints all the names defined by the compiled module hidden_4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    names.sort()
+    for name in names:
+        if not name.startswith("__"):
+            print(name)
