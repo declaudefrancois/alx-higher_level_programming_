24588e78904ef77d625ecbc68654cfbcaacccd19
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-05 18:25:07 -0800

    feat(0x03): switch values of two integers.

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    lst = my_list[:]
     if idx >= 0 and idx < len(my_list):
-        my_list = lst[:idx] + lst[idx + 1:]
+        my_list = my_list[:idx] + my_list[idx + 1:]
     return (my_list)
