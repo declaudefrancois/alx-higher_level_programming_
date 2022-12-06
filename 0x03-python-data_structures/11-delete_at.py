0e9ebd775141b3ba1a1acc4b1671d4a330168229
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-06 09:45:23 -0800

    fix(0x03): affect the original list inside the function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
-        my_list = my_list[:idx] + my_list[idx + 1:]
+        my_list[:] = my_list[:idx] + my_list[idx + 1:]
     return (my_list)
