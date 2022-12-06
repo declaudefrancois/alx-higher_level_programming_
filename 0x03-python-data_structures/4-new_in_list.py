fa65a74eaf3d396687045de93cb4775063037e13
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-05 16:25:15 -0800

    feat(0x03): replaces an element in a list at a specific position without modifying the original list (like in C).

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_cpy = my_list.copy()
+    if idx > 0 and idx < len(my_list):
+        my_list_cpy[idx] = element
+
+    return my_list_cpy
