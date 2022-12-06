c2d3c873b64345b35915a8b15de6d97bfc4f9ed4
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-06 15:49:38 -0800

    added tests cases

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -11,7 +11,9 @@ int main(void)
 {
 	listint_t *head;
 	listint_t *head1;
-
+	listint_t *head2;
+	listint_t *head3;
+	
 	head = NULL;
 	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 17);
@@ -35,10 +37,30 @@ int main(void)
 	head1 = NULL;
 	add_nodeint_end(&head1, 1);
 	add_nodeint_end(&head1, 2);
+	print_listint(head1);
 	if (is_palindrome(&head1) == 1)
                 printf("Linked list is a palindrome\n");
         else
                 printf("Linked list is not a palindrome\n");
+	free_listint(head1);
 
+	head2 = NULL;
+        print_listint(head2);
+        if (is_palindrome(&head2) == 1)
+                printf("Linked list is a palindrome\n");
+        else
+                printf("Linked list is not a palindrome\n");
+        free_listint(head2);
+
+	head3 = NULL;
+        add_nodeint_end(&head3, 1);
+        add_nodeint_end(&head3, 2);
+        add_nodeint_end(&head3, 1);
+        print_listint(head3);
+        if (is_palindrome(&head3) == 1)
+                printf("Linked list is a palindrome\n");
+        else
+                printf("Linked list is not a palindrome\n");
+        free_listint(head3);
 	return (0);
 }
