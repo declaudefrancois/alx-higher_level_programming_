9784310589759f50e3d15223ab1c0f658247aa1f
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-06 15:08:27 -0800

    feat(0x03): check if member values of nodes of a singly linked list form a palindrom

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: a pointer to the head of the list.
+ *
+ * Return:0 if it is not a palindrome, 1 if it is a palindrome.
+ */
+int is_palindrome(listint_t **head)
+{
+	int *arr = malloc(sizeof(int *));
+	int i = 0, size;
+	listint_t *h = *head;
+
+	if (arr == NULL)
+		return (0);
+
+	while (h)
+	{
+		arr[i++] = h->n;
+		h = h->next;
+	}
+
+	size = i;
+	--i;
+	for (int j = 0; j < size; j++)
+	{
+		if (arr[j] != arr[i--])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+
+	free(arr);
+	return (1);
+}
