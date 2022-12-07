96a67a19b3c25ac40618c197d50cd05237036bf2
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-06 16:39:20 -0800

    fix(0x03): fixed code style

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,19 +2,20 @@
 #include "Python.h"
 
 /**
- * print_python_list_info -  function that prints some basic info about Python lists.
- * @p: a pointer to a generic PyObject.
+ * print_python_list_info -  function that prints some basic
+ * info about Python lists.
  *
+ * @p: a pointer to a generic PyObject.
  */
 void print_python_list_info(PyObject *p)
 {
-        PyListObject *plo;
+	PyListObject *plo;
 	int i, size;
 
-        plo = (PyListObject *)p;
+	plo = (PyListObject *)p;
 	size = plo->ob_base.ob_size;
-        printf("[*] Size of the Python List = %ld\n", plo->ob_base.ob_size);
-        printf("[*] Allocated = %ld\n", plo->allocated);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", plo->allocated);
 
 	for (i = 0; i < size; i++)
 		printf("Element %d:%s\n", i, plo->ob_item[i]->ob_type->tp_name);
