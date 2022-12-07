3c5b54b7ee9a9f781c1b3f24f14aae16f882018e
Guy Francois de claude NKOUKA <declaudefrancois@gmail.com>
2022-12-07 00:34:33 +0000

    feat(0x03): C function that prints some basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "Python.h"
+
+/**
+ * print_python_list_info -  function that prints some basic info about Python lists.
+ * @p: a pointer to a generic PyObject.
+ *
+ */
+void print_python_list_info(PyObject *p)
+{
+        PyListObject *plo;
+	int i, size;
+
+        plo = (PyListObject *)p;
+	size = plo->ob_base.ob_size;
+        printf("[*] Size of the Python List = %ld\n", plo->ob_base.ob_size);
+        printf("[*] Allocated = %ld\n", plo->allocated);
+
+	for (i = 0; i < size; i++)
+		printf("Element %d:%s\n", i, plo->ob_item[i]->ob_type->tp_name);
+}
