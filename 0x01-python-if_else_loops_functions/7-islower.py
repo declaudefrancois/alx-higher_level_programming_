647ff33825f88d3b4b221252de25ab3931eabe4e
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-07 12:51:03 -0800

    fix(0x01): considered number as uppercase letters

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def islower(c):
-    return ((ord(c) >= ord('a') and ord(c) <= ord('z')) or
-            (ord(c) >= ord('0') and ord(c) <= ord('9')))
+    return (True if ord(c) >= ord('a') and ord(c) <= ord('z')
+            else False)
