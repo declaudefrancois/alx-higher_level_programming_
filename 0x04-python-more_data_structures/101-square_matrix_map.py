9e2aeaf66958bc18f5eff7331e24adde996183ab
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-08 02:54:35 +0100

    fix(0x04): used only map to pass the checker

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda mx: [i ** 2 for i in mx], matrix))
+    return list(map(lambda mx: list(map(lambda x: x ** 2, mx)), matrix))
