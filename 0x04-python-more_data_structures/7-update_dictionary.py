b4b09749c9c27534fcc63a90306a647de11ac2d3
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-08 02:09:08 +0100

    fix(0x03): removed list instance check to satisfy the checker

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if isinstance(a_dictionary, dict) != True:
-        a_dictionary = dict()
     a_dictionary[key] = value
     return a_dictionary
