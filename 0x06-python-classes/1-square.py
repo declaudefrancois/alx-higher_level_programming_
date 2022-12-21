e0bdd9ea4fe0c47a7201d598b21fbd4861c32fa3
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-20 16:13:23 -0800

    feat(0x06): classe with a private property

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """This class represents a geometrical square.
+
+    The square is a quadrilateral figure with each side equal to each others.
+
+    Attributes:
+        size (int): The size of the square.
+
+    """
+
+    def __init__(self, size=0):
+        """This method instanciates a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
