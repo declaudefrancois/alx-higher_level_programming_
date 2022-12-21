896986461d088ce955e8ff64eda41cc012bb277d
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-20 16:35:36 -0800

    feat(0x06): Square classe qith size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Square module."""
+
+
+class Square:
+    """This class represents a geometrical square.
+
+    The square is a quadrilateral figure with each side equal to each others.
+
+    Attributes:
+        size (int): The size of the square, a positive int.
+    """
+
+    def __init__(self, size=0):
+        """This method instanciates a new Square.
+        Args:
+            size (int): The size of the square.
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
