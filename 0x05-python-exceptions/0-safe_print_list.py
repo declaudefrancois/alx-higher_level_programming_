7d76cb3cda448423e0ce4d18765134e92b901cb1
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-26 23:09:57 -0800

    fix(0x05): printed empty line for x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,9 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="\n" if i == x - 1 else "",
                   sep="")
             count += 1
+
+        if x == 0:
+            print("")
         return (count)
     except IndexError:
         print("")
