dfb7ce23eb43b01882f58aa56e5ad5f199b14a5c
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2022-12-28 22:54:07 -0800

    feat(0x07): function for integers addition and test cases with doctest module

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Add module.
+    This module contains a simple function for adding
+    two numbers.
+"""
+
+
+def add_integer(a, b=98):
+    """ Adds two numbers of type inf or float.
+
+    Args:
+        a(Union[int, float]): The first number.
+        b(Union[int, float]): The second number.
+
+    Raises:
+        TypeError: If a or b is not an int or float.
+
+    Returns:
+        Union[int, float]: the result of a + b.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return (a + b)
