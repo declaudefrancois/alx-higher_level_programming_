fd584db1ed0d52f96f73feddb8f57cc0622358f8
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-02 11:06:28 -0800

    feat(0x08): added privates properties width and height, and setters and getters for both.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+    This module a single class representing
+    a rectangle.
+"""
+
+
+class Rectangle:
+    """
+        Represents a reactangle.
+
+        Args:
+            width (int): the reactangle width.
+            height (int): the rectangle height.
+
+        Attributes:
+            width (int): the reactangle width.
+            height (int): the reactangle height.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+            Instanciates a new Rectangle.
+
+            Args:
+                width (int): the reactangle width.
+                height (int): the rectangle height.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+            Getter of the rectangle's width.
+
+            Returns:
+                int: The rectangle's width.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """
+            Setter of the recatangle width.
+
+            Args:
+                width (int): the rectangle width to set.
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+            Getter of the rectangle's height.
+
+            Returns:
+                int: The rectangle's height.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """
+            Setter of the recatangle width.
+
+            Args:
+                height (int): the rectangle width to set.
+        """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = height
