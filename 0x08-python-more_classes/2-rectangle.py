223f6827701282da9e9c4b3a2920d2354fd7dbe2
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-03 02:29:56 -0800

    fix(0x08): perimeter equals 0 if width or height is equal to 0.

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -99,4 +99,7 @@ class Rectangle:
             Returns:
                 int: The rectangle instance perimeter.
         """
+        if self.width == 0 or self.height == 0:
+            return 0
+
         return 2 * (self.__width + self.__height)
