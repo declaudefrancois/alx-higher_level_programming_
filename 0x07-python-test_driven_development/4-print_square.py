c27c319fa02994daa9c99f9dba1c1a93c3d0014e
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-06 05:01:06 -0800

    feat(0x07): print_square functions and doc tests suite

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This module contains a single
+    function that print a square.
+"""
+
+
+def print_square(size):
+    """
+        Prints a square with the character #.
+
+        Args:
+            size (int): The length of a side.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
+
+    if size == 0:
+        print("")
