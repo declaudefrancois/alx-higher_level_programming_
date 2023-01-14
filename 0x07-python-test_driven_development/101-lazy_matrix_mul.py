3744f19b85c2ac9589916b5bd813be7854e852ef
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-14 06:23:31 -0800

    fix(0x07): documentation

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 from numpy import matmul
 
+"""
+    Matrix multiplication with
+    numpy.
+"""
 
 def lazy_matrix_mul(m_a, m_b):
-    return matmul(m_a, m_b).tolist()
+    """
+        Mutliplies two matrix using numpy.
+
+        Args:
+            m_a(list(list(int|float))): First matrix.
+            m_b(list(list(int|float))): First matrix.
+
+        Returns:
+            list(list(in|float)): m_a * m_b.
+    """
+    return matmul(m_a, m_b)
