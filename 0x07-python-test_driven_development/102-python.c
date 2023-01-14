5cb7b656f8429c45dd07dfb66d031d5f27f5ca65
Guy Francois de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-14 20:19:07 +0000

    feat(0x07): function to print info about a  Python strings.

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,35 @@
+#include "Python.h"
+
+/**
+ * print_python_string - prints Python strings.
+ * @p: A pointer to a generic PyObject
+ * representing a PyBytesObject which stores
+ * a python string.
+ */
+void print_python_string(PyObject *p)
+{
+	PyBytesObject *pso;
+	Py_ssize_t size;
+	printf("[.] string object info\n");	
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+	
+	pso = (PyBytesObject *) PyUnicode_AsUTF8String(p);
+	size =  pso->ob_base.ob_size;
+
+	printf("  type: ");
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("compact ascii\n");
+	else if (PyUnicode_IS_COMPACT(p))
+		printf("compact unicode object\n");
+	else if (PyUnicode_KIND(p) == PyUnicode_WCHAR_KIND)
+		printf("legacy string, not ready\n");
+	else
+		printf("legacy string, ready\n");
+
+	printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
+	printf("  value: %s\n", pso->ob_sval);
+}
