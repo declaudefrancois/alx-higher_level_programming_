b420cf30b1b56cf45c96a93fd101437c59a960fe
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-14 06:35:24 -0800

    fix(0x07): corrected doc tests.

--- 0x07-python-test_driven_development/101-main.py
+++ 0x07-python-test_driven_development/101-main.py
@@ -3,3 +3,7 @@ lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
 print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+
+m_a = [[1, 2], [3, 4]]
+m_b = [[5, 6], [7, 8]]
+print(lazy_matrix_mul(m_a, m_b))
