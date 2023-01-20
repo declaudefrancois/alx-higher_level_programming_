d02f9c0b144be7b0ac4f9b9feb96173bcd3730c5
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-20 03:45:16 -0800

    feat(0x0C): Square class that inherits from Rectanlge.

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    Contains the Square class.
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        A rectangle with each side equals to each
+        other.
+        Inherits Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            Instantiata a new Square object.
+
+            Args:
+                size (int): The length of a side.
+                x (int): horizontal coordinate in a 2d plan.
+                y (int): vertical coordinate in a 2d plan.
+                id (int): The id of the object.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        fmt = "[{}] ({}) {}/{} - {}"
+        return fmt.format(self.__class__.__name__, self.id, self.x,
+                          self.y, self.width)
