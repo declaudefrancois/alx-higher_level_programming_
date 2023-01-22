2d4a7ab68a8cc251410e0e752bc76cb7af0ef30f
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-01-22 00:47:59 +0000

    test(0x0C): test Base static method to_json_string

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,7 @@
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -29,3 +30,13 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id + 1, b2.id)
+
+    def test_to_json_string(self):
+        """
+            Should return the JSON of a list of dict.
+        """
+        r1 = Rectangle(10, 4, 8, 6, 1)
+
+        json_dict = Base.to_json_string([r1.to_dictionary()])
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(json_dict, '[{"id": 1, "width": 10, "height": 4, "x": 8, "y": 6}]')
