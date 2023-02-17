e2cfee51316577e1142ef95a6307924d097e6261
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-02-17 01:47:58 +0000

    (0x0E): 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+--  lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT c.id, c.state_id, c.name FROM 
+`cities` AS c, `states` AS s
+WHERE  s.name = "California"
+AND c.state_id = s.id
+ORDER BY c.id ASC;
