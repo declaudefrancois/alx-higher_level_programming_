2f7ddf58bc44cbcdeae0059cbbabf15fe53a0fb8
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-02-17 01:17:38 +0000

    (0x0E): 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa` CHARACTER SET utf8mb4
+       COLLATE utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
+
