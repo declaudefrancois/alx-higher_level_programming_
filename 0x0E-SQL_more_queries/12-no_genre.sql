1ae2f4c4281cbb661cee8e27ed3d2f4ee4fe1f9c
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-02-17 02:20:40 +0000

    (0x0E): 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshow.
+SELECT t.title, t1.genre_id
+FROM `tv_shows` AS t
+LEFT JOIN `tv_show_genres` AS t1
+ON t.id = t1.show_id
+WHERE t1.genre_id IS NULL
+ORDER BY t.title ASC, t1.genre_id ASC;
