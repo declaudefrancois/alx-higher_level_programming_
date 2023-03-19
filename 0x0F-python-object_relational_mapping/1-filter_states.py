680a425141ed362dc7d7b5f31923a1e4a88f9ef7
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-03-19 22:20:16 +0000

    feat(0x0F): filter states with MySQLdb.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Filter states query.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=user,
+        passwd=pswd,
+        db=db,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
