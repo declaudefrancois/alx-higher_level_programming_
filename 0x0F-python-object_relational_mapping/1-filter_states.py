2c80dc9d5f9f04138e7592c47c6622e5ee09ce48
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-03-19 22:31:02 +0000

    feat(0x0F): filter states by user input.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,20 +4,19 @@
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    user = sys.argv[1]
-    pswd = sys.argv[2]
-    db = sys.argv[3]
     conn = MySQLdb.connect(
         host="localhost",
-        port=3306, user=user,
-        passwd=pswd,
-        db=db,
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Get all states query.
+"""Filter states query.
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    pswd = sys.argv[2]
-    db = sys.argv[3]
     conn = MySQLdb.connect(
         host="localhost",
-        port=3306, user=user,
-        passwd=pswd,
-        db=db,
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         charset="utf8"
     )
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM `states` ORDER BY id ASC")
+    sql = "SELECT * FROM `states` WHERE `states`.`name` = '{}' ORDER BY id ASC"
+    cur.execute(sql.format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
