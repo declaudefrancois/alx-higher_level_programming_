8f98de2b87169987c10a955c32a9aeee12d321ca
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-07-06 03:01:41 +0100

    (0x10): finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -Lso /dev/null -w "%{size_download}"  0.0.0.0:5000 && echo
+curl -Lso /dev/null "$1" -w '%{size_download}'; echo ""
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Find a peak in a list of integers.
+"""Find a peak in a list of integers.
+"""
 
 
 def find_peak(list_int):
@@ -8,4 +9,18 @@ def find_peak(list_int):
        Params:
             list_int(list): A list of integers.
     """
-
+    limit = 1 + len(list_int) / 2
+    i = 0
+    peak = None
+    while i < limit and limit > 1:
+        low = list_int[i]
+        high = list_int[-i - 1]
+        if peak is None:
+            peak = low if low >= high else high
+        else:
+            if low >= high and low > peak:
+                peak = low
+            elif high >= low and high > peak:
+                peak = high
+        i += 1
+    return peak
